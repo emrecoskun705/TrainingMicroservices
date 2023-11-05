73397c6e3d76a1823626c3b1f5798edf95739c35
emrecoskun705 <emrecoskun705@gmail.com>
2023-11-05 11:38:15 +0300

    add docker for api gateways with ocelot

--- src/ApiGateways/OcelotApiGw/Startup.cs
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +19,12 @@ namespace OcelotApiGw
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +40,8 @@ namespace OcelotApiGw
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            await app.UseOcelot();
         }
     }
 }
